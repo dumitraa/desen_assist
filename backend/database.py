838d109332aa8ca735194829987a18c121770de1
dumitraa <112874229+dumitraa@users.noreply.github.com>
2025-05-01 12:59:18 +0300

    Switch database configuration from SQLite to PostgreSQL in alembic.ini and database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,12 @@
 from sqlmodel import create_engine, SQLModel
 
-engine = create_engine("sqlite:///backend/events.db", echo=False)
+# sqlite
+# engine = create_engine("sqlite:///backend/events.db", echo=False)
+engine = create_engine(
+    "postgresql+psycopg2://digitizer_api:test@localhost/digitizer_db",
+    pool_size=20, max_overflow=40, pool_pre_ping=True
+)
+
 def init_db():
     SQLModel.metadata.create_all(engine)
     
