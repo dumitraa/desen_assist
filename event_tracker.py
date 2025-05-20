8869b4daa667e805b4b3231b45089ebd249275bb
Dumitra <112874229+dumitraa@users.noreply.github.com>
2025-05-20 09:41:19 +0300

    feat: extend telemetry tracking

--- event_tracker.py
+++ event_tracker.py
@@ -1,14 +1,25 @@
 from qgis.core import QgsProject
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QObject, QTimer
 from .api_client import send_event
-from .config import LAYERS, NULL_VALUES          
+from .config import LAYERS, NULL_VALUES, IDLE_SECONDS
 
 class EditTracker(QObject):
     def __init__(self, iface, user_name):
         super().__init__()
         self.iface = iface
         self.user = user_name
+        self.checked_in = False
+        self.is_idle = False
+        self.idle_timer = QTimer(self)
+        self.idle_timer.setInterval(IDLE_SECONDS * 1000)
+        self.idle_timer.timeout.connect(self._on_idle_timeout)
+        self.idle_timer.start()
+
         self._connect_layers()
+        QgsProject.instance().layersAdded.connect(self._on_layers_added)
+        self.iface.projectRead.connect(self._on_project_opened)
+        self.iface.mapCanvas().extentsChanged.connect(self._reset_idle_timer)
+        send_event(user=self.user, action="session_start")
 
     # ----------------------------------------------------------------------
     def _connect_layers(self):
@@ -25,11 +36,15 @@ class EditTracker(QObject):
             layer.featureDeleted.connect(self.on_delete)
             layer.attributeValueChanged.connect(self.on_attr)
             layer.geometryChanged.connect(self.on_geom)
+            layer.editingStarted.connect(self.on_edit_start)
+            layer.editingStopped.connect(self.on_edit_stop)
 
     # ----------------------------------------------------------------------
     # Each slot uses self.sender() to know WHICH layer fired the signal ------
     def on_add(self, fid):
         lyr = self.sender()                         # QgsVectorLayer
+        self._check_in()
+        self._reset_idle_timer()
         send_event(
             user = self.user,
             layer  = lyr.name(),                   # ← STALP_JT etc.
@@ -39,6 +54,8 @@ class EditTracker(QObject):
 
     def on_delete(self, fid):
         lyr = self.sender()
+        self._check_in()
+        self._reset_idle_timer()
         send_event(
             user = self.user,
             layer  = lyr.name(),
@@ -49,6 +66,8 @@ class EditTracker(QObject):
     def on_attr(self, fid, idx, val):
         lyr        = self.sender()
         field_name = lyr.fields()[idx].name()       # turn index -> name
+        self._check_in()
+        self._reset_idle_timer()
 
         # example validation – forbid NULL in COD
         if field_name == "COD" and val in NULL_VALUES:
@@ -67,6 +86,8 @@ class EditTracker(QObject):
 
     def on_geom(self, fid, geom):
         lyr = self.sender()
+        self._check_in()
+        self._reset_idle_timer()
 
         # simple geometry validation
         if not geom.isSimple():
@@ -81,3 +102,60 @@ class EditTracker(QObject):
             fid    = fid,
             wkt    = geom.asWkt()
         )
+
+    # ----------------------------------------------------------------------
+    def on_edit_start(self):
+        lyr = self.sender()
+        self._check_in()
+        self._reset_idle_timer()
+        send_event(
+            user=self.user,
+            layer=lyr.name(),
+            action="edit_start",
+        )
+
+    def on_edit_stop(self):
+        lyr = self.sender()
+        send_event(
+            user=self.user,
+            layer=lyr.name(),
+            action="edit_stop",
+        )
+
+    def _on_layers_added(self, layers):
+        for layer in layers:
+            if layer.name() in LAYERS:
+                layer.featureAdded.connect(self.on_add)
+                layer.featureDeleted.connect(self.on_delete)
+                layer.attributeValueChanged.connect(self.on_attr)
+                layer.geometryChanged.connect(self.on_geom)
+                layer.editingStarted.connect(self.on_edit_start)
+                layer.editingStopped.connect(self.on_edit_stop)
+
+    def _on_project_opened(self):
+        send_event(
+            user=self.user,
+            action="project_open",
+            value=QgsProject.instance().fileName(),
+        )
+
+    def _on_idle_timeout(self):
+        if not self.is_idle:
+            self.is_idle = True
+            send_event(user=self.user, action="idle_start")
+
+    def _reset_idle_timer(self, *args):
+        if self.is_idle:
+            send_event(user=self.user, action="idle_end")
+            self.is_idle = False
+        self.idle_timer.start()
+
+    def _check_in(self):
+        if not self.checked_in:
+            send_event(user=self.user, action="check_in")
+            self.checked_in = True
+
+    def finalize(self):
+        if self.checked_in:
+            send_event(user=self.user, action="check_out")
+        send_event(user=self.user, action="session_end")
--- user_utils.py
+++ user_utils.py
@@ -1,6 +1,8 @@
 # user_utils.py ----------------------------------------------------------
-from qgis.PyQt.QtWidgets import QInputDialog # type: ignore
-from qgis.core import QgsSettings # type: ignore
+from qgis.PyQt.QtWidgets import QInputDialog  # type: ignore
+from qgis.core import QgsSettings  # type: ignore
+import configparser
+from pathlib import Path
 
 _SETTINGS_KEY = "/DesenAssist/UserName"
 
@@ -18,3 +20,11 @@ def get_current_user(parent=None) -> str:
             user = "unknown"
 
     return user
+
+
+def get_plugin_version() -> str:
+    """Return plugin version string from metadata.txt"""
+    meta_path = Path(__file__).with_name("metadata.txt")
+    parser = configparser.ConfigParser()
+    parser.read(meta_path, encoding="utf-8")
+    return parser.get("general", "version", fallback="0")
