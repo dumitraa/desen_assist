31fd042e299096415577c95ad0ecf07d7f1f94ee
dumitraa <112874229+dumitraa@users.noreply.github.com>
2025-05-29 11:41:32 +0300

    feat: add requirements and TODO list for backend enhancements

---
+++ backend/todo.py
@@ -0,0 +1,9 @@
+# TODO
+
+'''
+- only count km once if tronson_jt feature is overlapped
+- add user friendly naming for actions in the table
+- other layers other than the project layers = layer = 'Altele'
+- add ultima activitate in the user table
+
+'''
